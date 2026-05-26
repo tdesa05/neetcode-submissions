13548de8d2fdce98b3c72158693dfb0dfe3471be
Tyler De Sa <desa.tyler@gmail.com>
2026-05-26 13:03:00 +1000

    Add: trapping-rain-water - submission-6

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-6.cpp
@@ -0,0 +1,32 @@
+class Solution {
+   public:
+    int trap(std::vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int leftMax = 0;
+        int rightMax = 0;
+        int totalWater = 0;
+
+        while (left < right) {
+            // If the left height is smaller, we focus on it
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax)
+                    leftMax = height[left];  // New left max else {
+                else
+                    totalWater += leftMax - height[left];
+
+                left++;  // Move closer to right
+                         // Else, we are focusing on right height (basically does min(left,right)
+                         // functionality)
+            } else {
+                if (height[right] >= rightMax)
+                    rightMax = height[right];  // New right max
+                else
+                    totalWater += rightMax - height[right];
+                right--;  // Move closer to left
+            }
+        }
+
+        return totalWater;
+    }
+};
